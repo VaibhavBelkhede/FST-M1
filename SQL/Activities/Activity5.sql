d75ffa4b4388dfb2f0f00d69d1d5fd04dc205704
VaibhavBelkhede <130520141+VaibhavBelkhede@users.noreply.github.com>
2023-04-21 19:30:59 +0530

    Activity1 to Activity6 (SQL)
    Activity1 to Activity6 (SQL)

---
+++ SQL/Activities/Activity5.sql
@@ -0,0 +1,11 @@
+REM   Script: Activity5
+REM   Activity5
+
+UPDATE salesman SET grade=200 WHERE SALESMAN_CITY='Rome';
+
+UPDATE salesman SET grade=300 WHERE SALESMAN_NAME='James Hoog';
+
+UPDATE salesman SET SALESMAN_NAME='Pierre' WHERE SALESMAN_NAME='McLyon';
+
+SELECT * FROM salesman;
+
